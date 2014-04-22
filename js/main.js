911d151ce452c72ed8caa79414eced2021088605
Brendan Zhao <crickets147@gmail.com>
2014-04-22 19:59:17 -0400

    Created the banner and menu bar.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+  $(window).scroll(function () { 
+    if ($(window).scrollTop() >= 400) {
+      $('#nav').addClass('fixed');
+    }
+    else if ($(window).scrollTop() < 400) {
+      $('#nav').removeClass('fixed');
+    }
+  });
+});
