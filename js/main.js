417a83d7bd4cda85f8b310b142cdb397f84fc074
Brendan Zhao <crickets147@gmail.com>
2014-04-25 23:05:34 -0400

    Removed menu dependence on PureCSS grids and fixed menu jump bug

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $(window).scroll(function () { 
     if ($(window).scrollTop() >= $("header").height()) {
-      $('#nav').addClass('fixed');
+      $("#nav").addClass("fixed");
+      $("#nav-sticky").css("height", $("#nav").css("height"));
     }
     else if ($(window).scrollTop() < $("header").height()) {
-      $('#nav').removeClass('fixed');
+      $("#nav").removeClass("fixed");
+      $("#nav-sticky").css("height", "0");
     }
   });
 });
