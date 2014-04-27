483a0e4e52aa47d01fe8c94500d9638fd10426d1
Brendan Zhao <crickets147@gmail.com>
2014-04-27 07:53:40 -0400

    Purely asthetic changes. Most notably normalizing bullets across browsers.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $(window).scroll(function () { 
-    if ($(window).scrollTop() >= $("header").height()) {
+    if ($(window).scrollTop() > $("header").height()) {
       $("#nav").addClass("fixed");
       $("#nav-sticky").css("height", $("#nav").css("height"));
     }
-    else if ($(window).scrollTop() < $("header").height()) {
+    else if ($(window).scrollTop() <= $("header").height()) {
       $("#nav").removeClass("fixed");
       $("#nav-sticky").css("height", "0");
     }
